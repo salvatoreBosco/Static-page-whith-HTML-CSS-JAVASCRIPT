e2f2ca9915ae82ccaa7f63504db994d6fcb02452
salvatoreBosco <salvatorebosco.mail@gmail.com>
2022-09-21 02:26:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// This feature changes the bottom border 
+//of the navigation links based on the click
+function activeSection(p) {
+    const linkHome = document.getElementsByClassName('nav-item')[0]
+    const linkContact = document.getElementsByClassName('nav-item')[1]
+
+    if(p == 0){
+        linkHome.className = 'nav-item active';
+        linkContact.className = 'nav-item';
+    }else if(p == 1){
+        linkHome.className = 'nav-item';
+        linkContact.className = 'nav-item active';
+    }
+
+    document.addEventListener('wheel', (e)=>{
+        linkHome.className = 'nav-item active';
+        linkContact.className = 'nav-item active';
+    })
+}
+
+activeSection(0)
+
+
+
